09d6afc4acbf3794e99aee338e006710c31d9f1b
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-06-24 20:09:37 -0300

    feat(players): create "player.interface.ts".

---
+++ src/players/interfaces/player.interface.ts
@@ -0,0 +1,9 @@
+export interface Player {
+    readonly _id: string;
+    readonly phoneNumber: string;
+    readonly email: string;
+    name: string;
+    ranking: string;
+    positionRanking: number;
+    urlAvatarPlayer: string;
+}
