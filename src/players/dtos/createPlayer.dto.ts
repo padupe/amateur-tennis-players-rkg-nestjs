357692a266c0a8d40fbddfa9e6680a5c7dca644b
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-01 08:38:14 -0300

    refactor(src/players/dtos/createPlayer): use decoratos "class-validator".

--- src/players/dtos/createPlayer.dto.ts
+++ src/players/dtos/createPlayer.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator'
+
 export class CreatePlayerDTO {
+    @IsNotEmpty()
     readonly name: string
+
+    @IsEmail()
     readonly email: string
+
+    @IsNotEmpty()
     readonly phoneNumber: string
 }
