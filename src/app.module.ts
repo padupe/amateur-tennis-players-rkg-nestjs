b427503c6a93fb81677c7c2fccb381685fdc016a
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-05 08:45:57 -0300

    feat(src/app.module.ts): import "CategoriesModule. New Feature.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import 'dotenv/config'
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
+import { CategoriesModule } from './categories/categories.module'
 
 @Module({
     imports: [
         MongooseModule.forRoot(process.env.CONNECTION_MONGODB),
         PlayersModule,
+        CategoriesModule,
     ],
     controllers: [],
     providers: [],
