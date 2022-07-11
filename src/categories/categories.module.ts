71feb0c14ef6bf34ba806a444796c2e273ffc8b6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-11 11:34:08 -0300

    feat(src/categories/categories.module.ts): import "PlayersModule".

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,12 +3,14 @@ import { CategoriesController } from './categories.controller'
 import { CategoriesService } from './categories.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import { CategorySchema } from './interfaces/category.schema'
+import { PlayersModule } from 'src/players/players.module'
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: 'Category', schema: CategorySchema },
         ]),
+        PlayersModule,
     ],
     controllers: [CategoriesController],
     providers: [CategoriesService],
