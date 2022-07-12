a6641f908e24900d63d90722ee28e519e2472eba
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-12 08:53:24 -0300

    refactor(src/common/pipes): for use global at App.

--- src/common/pipes/validation-parameters.pipe.ts
+++ src/common/pipes/validation-parameters.pipe.ts
@@ -4,7 +4,7 @@ import {
     PipeTransform,
 } from '@nestjs/common'
 
-export class PlayersValidationParametersPipe implements PipeTransform {
+export class ValidationParametersPipe implements PipeTransform {
     transform(value: any, metadata: ArgumentMetadata) {
         if (!value) {
             throw new BadRequestException(
