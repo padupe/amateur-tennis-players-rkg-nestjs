72276e47cceb8f5608007e97541f3676387ab246
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-12 08:57:08 -0300

    feat(src/main.ts): "useGlobalFilters" at App.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { HttpExceptionFilters } from './common/filters/httpException.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.useGlobalFilters(new HttpExceptionFilters())
     await app.listen(8080)
 }
 bootstrap()
