5a7344a1506dcffe5219e9eadb086b34bbb16d46
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-18 08:51:13 -0300

    feat(src/main.ts): format Date strings.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { HttpExceptionFilters } from './common/filters/httpException.filter'
+import * as formatTimeZone from 'moment-timezone'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalFilters(new HttpExceptionFilters())
+
+    Date.prototype.toJSON = function (): any {
+        return formatTimeZone(this)
+            .tz('America/Sao_Paulo')
+            .format('YYYY-MM-DD HH:mm:ss.SSS')
+    }
+
     await app.listen(8080)
 }
 bootstrap()
