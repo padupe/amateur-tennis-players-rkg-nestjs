d744375c619d34dd0c3959cf5d571f15f6c832b6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-07-18 08:50:33 -0300

    feat(src/app.module.ts): import "ChallengesModule". New Feature.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PlayersModule } from './players/players.module'
 import { CategoriesModule } from './categories/categories.module'
+import { ChallengesModule } from './challenges/challenges.module'
 
 @Module({
     imports: [
         MongooseModule.forRoot(process.env.CONNECTION_MONGODB),
         PlayersModule,
         CategoriesModule,
+        ChallengesModule,
     ],
     controllers: [],
     providers: [],
